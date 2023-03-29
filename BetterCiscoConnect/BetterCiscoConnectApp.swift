a89cdab5fd63b569cce0c7771db81ff5c60d0b78
michaelpginn <milespergallon271@gmail.com>
2023-03-28 22:00:32 -0500

    Initial Commit

---
+++ BetterCiscoConnect/BetterCiscoConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterCiscoConnectApp.swift
+//  BetterCiscoConnect
+//
+//  Created by Michael Ginn on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterCiscoConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterCiscoConnect/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BetterCiscoConnect
+//
+//  Created by Michael Ginn on 3/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
